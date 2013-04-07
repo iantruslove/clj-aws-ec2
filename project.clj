25bd7473d6789c916af9ef5b066c5a4ceccc9da1
Ian Truslove <ian.truslove@gmail.com>
2013-04-06 21:08:32 -0600

    Updated clj-time version

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Clojure Amazon S3 library."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.amazonaws/aws-java-sdk "1.4.1"]
-                 [clj-time "0.4.4"]]
+                 [clj-time "0.5.0"]]
   :min-lein-version "2.0.0"
   :plugins [[codox "0.6.4"]])
